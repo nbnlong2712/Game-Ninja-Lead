4f57c3a2b315c6dff9e8cd4e1ca93e0a2c206987
nbnlong2712 <nhatlonghn1203@gmail.com>
2021-12-12 11:45:41 +0700

    Long: create enemy Lv3

---
+++ Assets/Scripts/Enemy3Bullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy3Bullet : MonoBehaviour
+{
+    [SerializeField] float speed = 35f;
+    [SerializeField] float timeToDestroy = 2f;
+    [SerializeField] GameObject explosion;
+    [SerializeField] int damage = 50;
+    void Start()
+    {
+        Invoke("DestroyBullet", timeToDestroy);
+    }
+
+    void Update()
+    {
+        //Vector2.up nghĩa là hướng nào thì bắn thẳng hướng đó
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<Player>().TakeDamage(damage);
+            DestroyBullet();
+        }
+    }
+
+    void DestroyBullet()
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/EnemyLV3.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLV3 : Enemy
+{
+    [SerializeField] float stopDistance = 5f;
+    private float attackTime;
+    Player player;
+    Animator animator;
+    [SerializeField] Transform shotPoint;
+    [SerializeField] GameObject bullet;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        player = FindObjectOfType<Player>();
+    }
+
+    void Update()
+    {
+        if(player != null)
+        {
+            if(Vector2.Distance(transform.position, player.transform.position) > stopDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+            }
+            else
+            {
+                if (Time.time > attackTime)
+                {
+                    attackTime += Time.time + timeBetweenAttack;
+                    animator.SetTrigger("attack");
+                }
+            }
+        }
+    }
+
+    public void Attack()
+    {
+        if (player != null)
+        {
+            Vector2 direction = player.transform.position - shotPoint.position;    //direction: hướng bắn: muốn tính hướng bắn, lấy hai vector trừ nhau (toán lớp 10)
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+            shotPoint.rotation = rotation;
+            Instantiate(bullet, shotPoint.position, shotPoint.rotation);
+        }
+    }
+
+    public override void TakeDamage(float damage)
+    {
+        base.TakeDamage(damage);
+    }
+
+    public override void Die()
+    {
+        base.Die();
+        animator.SetBool("isDeath", true);
+    }
+}
